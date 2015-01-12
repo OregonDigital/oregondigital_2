49e9980a630ed6c20e09c1e5e65d6e42a47dfd37
Trey Terrell <trey.terrell@oregonstate.edu>
2015-01-12 12:30:28 -0800

    Remove oregon_digital require.

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require 'oregon_digital'
 
 module OregonDigital
   class Application < Rails::Application
