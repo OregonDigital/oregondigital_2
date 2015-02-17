277bcf411473b2ac9dab3db3a84cea2e09fbce3a
Trey Terrell <trey.terrell@oregonstate.edu>
2015-02-16 20:18:29 -0800

    Simplify streamable_content spec.

--- spec/lib/streamable_content_spec.rb
+++ spec/lib/streamable_content_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe StreamableContent do
   verify_contract :streamable_content
-  subject { StreamableContent.new(string, mime_type) }
-  let(:string) { "bla" }
-  let(:mime_type) { "image/jpeg" }
   describe "#path" do
-    let(:result) { subject.path }
-    context "when given something with a mime_type of 'image/jpeg'" do
+    context "when it has a mime_type of image/jpeg" do
       it "should be test_item.jpg" do
-        expect(result).to eq "test_item.jpg"
+        content = StreamableContent.new("bla", "image/jpeg")
+
+        expect(content.path).to eq "test_item.jpg"
       end
     end
   end
