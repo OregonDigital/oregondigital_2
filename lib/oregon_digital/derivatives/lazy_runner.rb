550bddc90d71588609a337613a5fd82ccb08ae8a
Trey Terrell <trey.terrell@oregonstate.edu>
2015-02-19 11:21:02 -0800

    A little description for LazyRunner.

--- lib/oregon_digital/derivatives/lazy_runner.rb
+++ lib/oregon_digital/derivatives/lazy_runner.rb
@@ -1,3 +1,7 @@
+##
+# This class finds runners for a given asset, but ONLY when you try to use it as
+# a runner. This is required because assets don't have an ID until
+# post-persistence, and AssetWithDerivatives needs runners at instantiation.
 module OregonDigital::Derivatives
   class LazyRunner
     attr_reader :asset, :runner_finder
