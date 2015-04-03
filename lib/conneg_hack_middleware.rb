57bec70c23b9c0c0dd48c17c5f67263eb48fca8b
Trey Terrell <trey.terrell@oregonstate.edu>
2015-04-03 15:20:42 -0700

    Add middleware to fix bug in Rails
    Content negotiation is broken in Rails - see
    https://github.com/rails/rails/issues/9940

---
+++ lib/conneg_hack_middleware.rb
@@ -0,0 +1,10 @@
+class ConnegHackMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    env["HTTP_ACCEPT"] = env["HTTP_ACCEPT"].to_s.gsub(/, ?\*\/\*(;q=.*)?$/,'')
+    @app.call(env)
+  end
+end
