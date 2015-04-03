90c42c3947536acc396188b948c59dbbc0583402
Trey Terrell <trey.terrell@oregonstate.edu>
2015-04-03 08:53:32 -0700

    Fix Blacklight update.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -5,6 +5,7 @@ class CatalogController < ApplicationController
 
   include Blacklight::Catalog
   include Hydra::Controller::ControllerBehavior
+  include Hydra::Controller::SearchBuilder
   # This applies appropriate access controls to all solr queries
   self.search_params_logic += [:add_access_controls_to_solr_params]
   # Include Oregon Digital Catalog Logic
