a5442a82772ca2f15c5537ef8fb4343fb6dc3d25
Jeremy Echols <jechols@uoregon.edu>
2015-04-13 11:47:27 -0700

    Set up hydra editor

---
+++ app/forms/generic_asset_form.rb
@@ -0,0 +1,7 @@
+class GenericAssetForm
+  include HydraEditor::Form
+  # TODO: Put :workType back in once our editor can support URLs and/or we have
+  # a "clean" graph from AF
+  skip_terms = [:workType]
+  self.terms = ODDataModel.properties.keys - skip_terms
+end
---
+++ app/forms/image_form.rb
@@ -0,0 +1,3 @@
+class ImageForm < GenericAssetForm
+  self.model_class = Image
+end
