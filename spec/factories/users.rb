4abe10db7b1f28b9e9beeb5cfde69f9364357560
Jeremy Echols <jechols@uoregon.edu>
2015-04-14 13:39:16 -0700

    Add test for hydra editor

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,4 +3,10 @@ FactoryGirl.define do
     
   end
 
+  factory :admin, :class => User do
+    id 1
+    email "admin@example.org"
+    password "admin123"
+    password_confirmation "admin123"
+  end
 end
