3e1047dea6879feffb0f7a806ec7d3a5b38f9181
balesh2 <balesh@onid.oregonstate.edu>
2015-04-20 14:36:33 -0700

    adds admin panel and injest new record and tests

---
+++ spec/views/admin/index.html.erb_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'admin/index' do
+  let(:blacklight_config) { CatalogController.new.blacklight_config }
+  let(:ability) { Ability.new(user) }
+  before do
+    allow(view).to receive(:blacklight_config).and_return(blacklight_config)
+    allow(view).to receive(:has_user_authentication_provider?).and_return(true)
+    allow(view).to receive(:current_user).and_return(user) if user
+    allow(controller).to receive(:current_ability).and_return(ability)
+    render
+  end
+  context "when not logged in" do
+    let(:user) {nil}
+    it "should display an insufficient permissions error" do
+      expect(rendered).to_not have_content "Admin Panel"
+      expect(rendered).to have_content "You do not have sufficient permissions to access this page."
+    end
+  end
+  context "when logged in as a user" do
+    let(:user) { FactoryGirl.build(:user) }
+    it "should display an insufficient permissions error" do
+      expect(rendered).to_not have_content "Admin Panel"
+      expect(rendered).to have_content "You do not have sufficient permissions to access this page."
+    end
+  end
+  context "when logged in as an admin" do
+    let(:user) { FactoryGirl.build(:admin) }
+    it "should display the admin panel" do
+      expect(rendered).to have_content "Admin Panel"
+    end
+    context "when ingesting a new record" do
+      it "should have a link to injest a new record" do
+        expect(rendered).to have_link "Ingest a New Record", :href => hydra_editor.new_record_path
+     end
+    end
+  end
+end
