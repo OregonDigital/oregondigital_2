e4d81e51de9a5ec22458090851bbf4ad4fe8e3ad
Trey Terrell <trey.terrell@oregonstate.edu>
2015-04-21 13:58:34 -0700

    Add preferred label discovery.
    This finds all english labels and selects the first one, if possible. Useful for
    indexing.

--- spec/models/triple_powered_resource_spec.rb
+++ spec/models/triple_powered_resource_spec.rb
@@ -42,4 +42,29 @@ RSpec.describe TriplePoweredResource do
       expect(reloaded_resource.statements.to_a.first.subject).to eq subject.rdf_subject
     end
   end
+
+  describe "#preferred_label" do
+    context "when there's a french and english label" do
+      before do
+        subject << [subject.rdf_subject, RDF::SKOS.prefLabel, RDF::Literal("French", :language => :fr)]
+        subject << [subject.rdf_subject, RDF::SKOS.prefLabel, RDF::Literal("Test", :language => :en)]
+      end
+      it "should prefer the english version" do
+        expect(subject.preferred_label).to eq "Test"
+      end
+    end
+    xit "should delegate to LabelFinder" do
+      label_finder = instance_double(LabelFinder)
+      label = double("Label")
+      allow(LabelFinder).to receive(:new).with(subject, anything).and_return(label_finder)
+      allow(label_finder).to receive(:label).and_return(label)
+
+      expect(subject.preferred_label).to eq label
+    end
+    it "should work" do
+      subject.preflabel = ["Test"]
+      
+      expect(subject.preferred_label).to eq "Test"
+    end
+  end
 end
