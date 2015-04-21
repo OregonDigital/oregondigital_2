dbe2c7b2afeb8c2540b78027deea9ff8e540c0c0
Trey Terrell <trey.terrell@oregonstate.edu>
2015-04-21 09:31:11 -0700

    Use already written subject changer.

--- app/repositories/fedora_ldp_repository.rb
+++ app/repositories/fedora_ldp_repository.rb
@@ -5,9 +5,25 @@ class FedoraLDPRepository
   end
 
   def <<(stuff)
-    temp_graph = SubjectMutater.new(stuff, self.subject, absolute_path).graph
+    temp_graph = subject_changer.call(stuff, self.subject, absolute_path)
     resource.graph << temp_graph
     resource.save
+  end
+
+  def query(query_params)
+    subject_changer.call(resource.graph,
+                        connection.http.url_prefix.to_s,
+                        self.subject)
+  end
+
+  def delete(query_to_delete)
+    true
+  end
+
+  private
+
+  def subject_changer
+    OregonDigital::GraphMutators::SubjectChanger
   end
 
   def relative_path
@@ -18,36 +34,12 @@ class FedoraLDPRepository
     RDF::URI(connection.http.url_prefix.to_s+"/"+relative_path)
   end
 
-  def query(query_params)
-    SubjectMutater.new(resource.graph,
-                       connection.http.url_prefix.to_s,
-                       self.subject).graph
-  end
 
   def resource
     @resource ||= Ldp::Resource::RdfSource.new(connection, relative_path, nil, ActiveFedora.fedora.base_path)
   end
 
-  def delete(query_to_delete)
-    true
-  end
-
   def connection
     @connection ||= ActiveFedora.fedora.connection
-  end
-end
-
-class SubjectMutater
-  pattr_initialize :source_graph, :find, :replace
-  def graph
-    temp_graph = RDF::Graph.new
-    source_graph.statements.to_a.each do |statement|
-      s = statement.subject
-      if s.to_s.start_with?(find.to_s)
-        s = replace
-      end
-      temp_graph << [s, statement.predicate, statement.object]
-    end
-    temp_graph
   end
 end
