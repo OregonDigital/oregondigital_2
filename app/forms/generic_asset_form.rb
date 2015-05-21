443c9efe3fb0965e00832a1e918e6e0e599f10ec
Brandon Straley <straleyb@STRALEYB-MBP15.tss.oregonstate.edu>
2015-05-21 13:14:07 -0700

    Pointing to jechols branch for hydra editor

--- app/forms/generic_asset_form.rb
+++ app/forms/generic_asset_form.rb
@@ -2,8 +2,6 @@ class GenericAssetForm
   include HydraEditor::Form
   # TODO: Put :workType back in once our editor can support URLs and/or we have
   # a "clean" graph from AF
-  #
- 
   skip_terms = [:workType]
   self.terms = ODDataModel.simple_properties.keys - skip_terms
   self.factory = OregonDigital::Fields::InputFactory
@@ -11,5 +9,4 @@ class GenericAssetForm
   def self.permitted_params
     super << :content
   end
-
 end
