ad97cc86c52f10817ccae8cefc73d280c7884efb
Brandon Straley <straleyb@STRALEYB-MBP15.tss.oregonstate.edu>
2015-05-27 10:01:50 -0700

    Fixed tests for input factory to use let

--- spec/forms/oregon_digital/fields/input_factory_spec.rb
+++ spec/forms/oregon_digital/fields/input_factory_spec.rb
@@ -2,35 +2,22 @@ require 'rails_helper'
 
 RSpec.describe OregonDigital::Fields::InputFactory do
   subject { described_class.new(base_factory, decorator) }
-  let(:base_factory) { build_input_factory }
-  let(:decorator) { build_decorator }
+  let(:base_factory) { object_double(HydraEditor::Fields::Factory) }
+  let(:decorator) { object_double(HasHintOption) }
+  let(:object) { instance_double(GenericAssetForm) }
   let(:decorated_input) { double("decorated input") }
   let(:input) { double("input") }
+  let(:property) { :lcsubject }
+  let(:result) { subject.create(object, property) }
 
   describe "#create" do
     it "decorate the input" do
-      object = build_object
-
-      result = subject.create(object, :lcsubject)
+      allow(base_factory).to receive(:create).and_return(input)
+      allow(decorator).to receive(:new).and_return(decorated_input)
 
       expect(result).to eq decorated_input
       expect(decorator).to have_received(:new).with(input)
       expect(base_factory).to have_received(:create).with(object, :lcsubject)
     end
-
-    def build_object
-      instance_double(GenericAssetForm)
-    end
-  end
-
-  def build_input_factory
-    f = object_double(HydraEditor::Fields::Factory)
-    allow(f).to receive(:create).and_return(input)
-    f
-  end
-  def build_decorator
-    f = object_double(HasHintOption)
-    allow(f).to receive(:new).and_return(decorated_input)
-    f
   end
 end
