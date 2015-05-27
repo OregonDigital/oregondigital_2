599747acf52f0c9ada9f16a78b508c036f24a0cc
Jeremy Echols <jechols@uoregon.edu>
2015-05-27 10:49:57 -0700

    Don't load simplecov if we aren't using it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,12 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 $LOAD_PATH << '../lib'
-require 'simplecov'
-SimpleCov.start 'rails' unless ENV['NO_COVERAGE']
+
+unless ENV['NO_COVERAGE']
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
