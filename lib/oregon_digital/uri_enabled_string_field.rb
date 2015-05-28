cf5c951027209d61e4d62f472c40675641597dca
Jeremy Echols <jechols@uoregon.edu>
2015-05-28 11:20:28 -0700

    Use pattr_initialize for URIEnabledStringField

--- lib/oregon_digital/uri_enabled_string_field.rb
+++ lib/oregon_digital/uri_enabled_string_field.rb
@@ -3,17 +3,13 @@ module OregonDigital
   # while still being able to be removed.  Called from a simpleform input to
   # generate HTML for the view.
   class URIEnabledStringField
-    def initialize(builder, attribute_name, options)
-      @builder = builder
-      @attribute_name = attribute_name
-      @options = options
-    end
+    pattr_initialize :builder, :attribute_name, :options
 
     def field
-      if @options[:value].respond_to?(:rdf_label)
+      if options[:value].respond_to?(:rdf_label)
         rdf_field
       else
-        @builder.text_field(@attribute_name, @options)
+        builder.text_field(attribute_name, options)
       end
     end
 
@@ -25,7 +21,7 @@ module OregonDigital
 
     # Returns effectively a dummy field just for displaying a friendly label
     def rdf_text_field
-      @builder.text_field(@attribute_name, @options.merge(
+      builder.text_field(attribute_name, options.merge(
         :id => nil,
         :name => nil,
         :value => label,
@@ -35,15 +31,15 @@ module OregonDigital
 
     # Returns the actual important RDF data: the URI
     def rdf_uri_field
-      @builder.hidden_field(@attribute_name, @options.merge(:value => subject))
+      builder.hidden_field(attribute_name, options.merge(:value => subject))
     end
 
     def label
-      @options[:value].rdf_label.first.to_s
+      options[:value].rdf_label.first.to_s
     end
 
     def subject
-      @options[:value].rdf_subject.to_s
+      options[:value].rdf_subject.to_s
     end
   end
 end
