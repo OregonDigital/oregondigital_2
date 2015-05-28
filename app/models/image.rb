8f43367e6f9fefeba536f08fde8072575ab0ec3b
luisgreg99 <luisram90_101@hotmail.com>
2015-05-28 14:18:02 -0700

    added view for image viewer

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,11 @@
 class Image < GenericAsset
   has_derivatives :thumbnail, :medium, :pyramidal
 
+  def relative_image_location(abs_path)
+    base_path = Pathname.new(Rails.root)
+    file_path = Pathname.new(abs_path)
+    return "/" + file_path.relative_path_from(base_path).to_s
+
+  end
+
 end
