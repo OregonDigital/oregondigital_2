1358127528f21a380690b43fc9928d940136229e
luisgreg99 <luisram90_101@hotmail.com>
2015-05-29 13:45:18 -0700

    added spec to test small_image_viewer

--- spec/views/catalog/_show_default.html.erb_spec.rb
+++ spec/views/catalog/_show_default.html.erb_spec.rb
@@ -5,16 +5,19 @@ RSpec.describe "catalog/_show_default.html.erb" do
     GenericAsset.new do |g|
       g.attributes = {
         :title => ["Title"],
-        :alternative => ["Test"]
+        :alternative => ["Test"],
       }
     end
   end
+
+  let(:file) { File.open(File.join(fixture_path, 'fixture_image.jpg'), 'rb') }
   let(:blacklight_config) {
     BlacklightConfig.new(GenericAsset, CatalogController.blacklight_config).configuration
   }
   let(:document) { SolrDocument.new(resource.to_solr) }
   before do
     allow(view).to receive(:blacklight_config).and_return(blacklight_config)
+
     render :partial => "catalog/show_default.html.erb", :locals => {:document => document }
   end
   it "should show the title" do
@@ -23,5 +26,20 @@ RSpec.describe "catalog/_show_default.html.erb" do
   it "should show alternative" do
     expect(rendered).to have_content("Alternative")
     expect(rendered).to have_content("Test")
+  end
+
+  describe "catalog/_small_image_viewer" do
+    context "when an image with jpeg content" do
+
+      before do 
+        resource.add_file(file, :path => "content")
+      end
+      it "should have a basic image tag" do
+        expect(response).to render_template(:partial => '_small_image_viewer')
+        expect(rendered).to have_selector("#small-image-viewer")
+        # expect(rendered).to have_selector("img")
+      end
+
+    end
   end
 end
