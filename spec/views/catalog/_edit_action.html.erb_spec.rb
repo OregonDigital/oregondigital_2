87d8ffd3a18388e0576c9d3d84b4e67476970114
balesh2 <balesh@onid.oregonstate.edu>
2015-05-29 10:08:26 -0700

    gets rid of excess objects

--- spec/views/catalog/_edit_action.html.erb_spec.rb
+++ spec/views/catalog/_edit_action.html.erb_spec.rb
@@ -10,14 +10,10 @@ RSpec.describe "catalog/_edit_action.html.erb" do
       }
     end
   end
-  let(:blacklight_config) {
-    BlacklightConfig.new(GenericAsset, CatalogController.blacklight_config).configuration
-  }
   let(:document) { SolrDocument.new(resource.to_solr) }
   let(:ability) { Ability.new(user) }
   before do
     allow(controller).to receive(:current_ability).and_return(ability)
-    allow(view).to receive(:blacklight_config).and_return(blacklight_config)
     render :partial => "catalog/edit_action.html.erb", :locals => {:document => document }
   end
   context "when not logged in" do
