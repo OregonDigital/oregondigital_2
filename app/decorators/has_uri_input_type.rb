e730d6947a5a55cffdffc4102796780fd3c090ad
Jeremy Echols <jechols@uoregon.edu>
2015-05-29 10:37:37 -0700

    Don't override single-value fields' "as" value

--- app/decorators/has_uri_input_type.rb
+++ app/decorators/has_uri_input_type.rb
@@ -1,12 +1,13 @@
 # Decorator for OregonDigital::Fields::InputFactory to allow the URI-enabled
-# input type
+# input type.
+#
+# @note We purposely don't allow single-value URI-enabled fields for now, and
+# those will just fall through here with no changes.
 class HasURIInputType < SimpleDelegator
   def options
     opts = super.dup
     if object.class.multiple?(property)
       opts[:as] = :uri_multi_value
-    else
-      opts[:as] = :string
     end
     opts
   end
