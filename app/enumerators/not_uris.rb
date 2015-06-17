8842ac5af3f488abef1896418bbfe4a1a52f1a07
Trey Terrell <trey.terrell@oregonstate.edu>
2015-06-17 16:06:34 -0700

    Use enumerator and solr document interface.

---
+++ app/enumerators/not_uris.rb
@@ -0,0 +1,24 @@
+##
+# An enumerator which iterates only over non-URIs in a given set of values.
+class NotUris
+  attr_reader :values
+  delegate :length, :present?, :blank?, :to => :to_a
+  include Enumerable
+  def initialize(values)
+    @values = Array.wrap(values)
+  end
+
+  def each
+    values.each do |value|
+      if !uri?(value)
+        yield value
+      end
+    end
+  end
+
+  private
+
+  def uri?(value)
+    MaybeURI.new(value).uri?
+  end
+end
--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -5,19 +5,11 @@ class SolrDocument
 
   use_extension(Hydra::ContentNegotiation)
 
-  def filtered_field_values(field)
-    Array(self[field]).map do |value|
-      uri_filter(value).filter
-    end.compact
-  end
-
-  def field_value_is_uri?(field)
-    filtered_field_values(field).empty?
-  end
- 
-  private
-
-  def uri_filter(value)
-    OregonDigital::URIFilter.new(value, MaybeURI) 
+  def [](value)
+    if value != "id"
+      NotUris.new(super)
+    else
+      super
+    end
   end
 end
