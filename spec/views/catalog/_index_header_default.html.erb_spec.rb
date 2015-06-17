b35f59d71e74c82e263a9c2d62de7e7d8aac18f9
Trey Terrell <trey.terrell@oregonstate.edu>
2015-06-17 11:55:46 -0700

    Extract methods.

--- spec/views/catalog/_index_header_default.html.erb_spec.rb
+++ spec/views/catalog/_index_header_default.html.erb_spec.rb
@@ -17,15 +17,23 @@ RSpec.describe "catalog/_index_header_default" do
 
   before do
     assign :response, double(start: 0)
-    allow(view).to receive(:render_grouped_response?).and_return false
-    allow(view).to receive(:blacklight_config).and_return(blacklight_config)
-    allow(view).to receive(:current_search_session).and_return nil
-    allow(view).to receive(:search_session).and_return({})
-    allow(view).to receive(:render_index_doc_actions)
     assign(:set, set)
+    stub_helper_methods
+    stub_blacklight_config(config: blacklight_config)
     render :partial => "catalog/index_header_default", :locals => {:document => document, :document_counter => 1}
   end
   it "should display a link, but stay in the sets path" do
     expect(rendered).to have_link document.id, :href => sets_path(:id => document.id, :set_id => set.id)
+  end
+
+  def stub_blacklight_config(config:)
+    allow(view).to receive(:blacklight_config).and_return(config)
+  end
+
+  def stub_helper_methods
+    allow(view).to receive(:render_grouped_response?).and_return false
+    allow(view).to receive(:current_search_session).and_return nil
+    allow(view).to receive(:search_session).and_return({})
+    allow(view).to receive(:render_index_doc_actions)
   end
 end
