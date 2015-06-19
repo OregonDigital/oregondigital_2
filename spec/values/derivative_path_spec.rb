6d68a5ca4ffe5461846fc160d6035ac24b036566
Trey Terrell <trey.terrell@oregonstate.edu>
2015-06-19 15:07:01 -0700

    Add a way to get what derivative the path is for.

--- spec/values/derivative_path_spec.rb
+++ spec/values/derivative_path_spec.rb
@@ -15,5 +15,26 @@ RSpec.describe DerivativePath do
     it "should return a relative path" do
       expect(subject.relative_path).to eq Pathname.new("/media/thumbnails/t/s/test.jpg")
     end
+    context "when instantiated with a string path" do
+      let(:path) { injector.thumbnail_path("test").to_s }
+      it "should work" do
+        expect(subject.relative_path).to eq Pathname.new("/media/thumbnails/t/s/test.jpg")
+      end
+    end
+  end
+
+  describe "#derivative_type" do
+    let(:result) { subject.derivative_type }
+    context "given a thumbnail" do
+      it "should return :thumbnail" do
+        expect(result).to eq :thumbnail
+      end
+    end
+    context "given a pyramidal" do
+      let(:path) { injector.pyramidal_path("test") }
+      it "should return a pyramidal" do
+        expect(result).to eq :pyramidal
+      end
+    end
   end
 end
