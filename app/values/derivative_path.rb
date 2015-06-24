a298b69098b25653390377c8fde8b3e689d3b496
Jeremy Echols <jechols@uoregon.edu>
2015-06-24 14:07:28 -0700

    Add to_iiif for getting IIIF info.json path

--- app/values/derivative_path.rb
+++ app/values/derivative_path.rb
@@ -11,12 +11,24 @@ class DerivativePath < SimpleDelegator
     path_factory.new("/").join(relative_path_from(derivative_base))
   end
 
+  def relative_path_no_slash
+    relative_path.to_s.sub(%r|^\/|, '')
+  end
+
   def derivative_base
     path_factory.new(injector.derivative_base.parent)
   end
 
   def derivative_type
     relative_path_from(path_factory.new(injector.derivative_base)).to_s.split("/").first.singularize.underscore.to_sym
+  end
+
+  def to_iiif
+    path_factory.new(OregonDigital.iiif_server_url).join(escape_slashes(relative_path_no_slash), "info.json")
+  end
+
+  def escape_slashes(s)
+    s.gsub("/", "%2F")
   end
 
   private
