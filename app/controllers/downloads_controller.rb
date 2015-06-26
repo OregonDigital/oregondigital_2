9ad16f8d9dc3773a9929d08fe0042d1e34337623
luisgreg99 <luisram90_101@hotmail.com>
2015-06-26 14:32:58 -0700

    updated Download controller to set proper file mime type

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -3,4 +3,12 @@
 # Hydra::Controller::DownloadBehavior can be overridden.
 class DownloadsController < ApplicationController
   include Hydra::Controller::DownloadBehavior
+
+  def prepare_file_headers
+    send_file_headers! content_options
+    file.mime_type = "image/jpeg"
+    response.headers['Content-Type'] = file.mime_type
+    self.content_type = file.mime_type
+  end
+
 end
