c3bd1b8a4a431cae62716525530f4c756c200747
Trey Terrell <trey.terrell@oregonstate.edu>
2015-07-02 14:31:47 -0700

    Refactor.

--- app/controllers/admin/facets_controller.rb
+++ app/controllers/admin/facets_controller.rb
@@ -1,5 +1,15 @@
 class Admin::FacetsController < AdminController
   def index
-    @solr_fields = FacetableSolrFieldIterator.new(SolrFieldSummary.where(:field => "*"))
+    @solr_fields = iterator_factory.new(all_solr_fields)
+  end
+
+  private
+
+  def all_solr_fields
+    SolrFieldSummary.where(:field => "*")
+  end
+
+  def iterator_factory
+    FacetableSolrFieldIterator
   end
 end
