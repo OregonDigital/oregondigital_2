718964e338eaf6f7ffc1d1312cac90ad3e7bfd0b
Trey Terrell <trey.terrell@oregonstate.edu>
2015-07-02 16:29:12 -0700

    Add missing spec.

--- spec/routing/admin_facet_routing_spec.rb
+++ spec/routing/admin_facet_routing_spec.rb
@@ -7,4 +7,7 @@ RSpec.describe "facet admin routing" do
   it "should route post /admin/facets to facets#create" do
     expect(post '/admin/facets').to route_to :controller => "admin/facets", :action => "create"
   end
+  it "should route delete /admin/facet/1 to facets#destroy" do
+    expect(delete '/admin/facets/1').to route_to :controller => "admin/facets", :action => "destroy", :id => "1"
+  end
 end
