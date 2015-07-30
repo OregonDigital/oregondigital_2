4a6242221fd620f7349063c7e5e6d250b8e7bff0
Trey Terrell <trey.terrell@oregonstate.edu>
2015-07-30 12:33:19 -0700

    Add MediaSpace configuration.

--- app/forms/external_asset_form.rb
+++ app/forms/external_asset_form.rb
@@ -1,4 +1,4 @@
 class ExternalAssetForm < GenericAssetForm
   self.model_class = ExternalAsset
-  self.terms += [:oembed]
+  self.terms = [:oembed] + self.terms
 end
--- spec/forms/external_asset_form_spec.rb
+++ spec/forms/external_asset_form_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ExternalAssetForm do
   describe ".terms" do
     it "should include oembed" do
-      expect(described_class.terms).to include :oembed
+      expect(described_class.terms.first).to eq :oembed
     end
   end
 end
