1405ea8bfa54006dc03b7f75074b37a51132c8be
Trey Terrell <trey.terrell@oregonstate.edu>
2015-07-30 14:19:03 -0700

    Add more working URLs for Mediaspace

--- config/initializers/oembed_providers.rb
+++ config/initializers/oembed_providers.rb
@@ -5,5 +5,7 @@ OEmbed::Providers.register_all
 ## Register OSU Mediaspace Provider
 mediaspace_provider = OEmbed::Provider.new("https://media.oregonstate.edu/oembed")
 mediaspace_provider << "https://media.oregonstate.edu/id*"
-mediaspace_provider << "http.://media.oregonstate.edu/id*"
+mediaspace_provider << "http://media.oregonstate.edu/id*"
+mediaspace_provider << "http://media.oregonstate.edu/media/id*"
+mediaspace_provider << "https://media.oregonstate.edu/media/id*"
 OEmbed::Providers.register(mediaspace_provider)
