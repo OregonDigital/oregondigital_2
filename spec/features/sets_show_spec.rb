1cd8e6d24ce4e249f14e9fe8e928b64424dc1f16
Trey Terrell <trey.terrell@oregonstate.edu>
2015-08-04 09:16:35 -0700

    Feature spec in CI only.

--- spec/features/sets_show_spec.rb
+++ spec/features/sets_show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Sets show page" do
+RSpec.describe "Sets show page", :slow => true do
   it "should be able to display an asset" do
     set = GenericSet.create(:read_groups => ["public"])
     asset = GenericAsset.create(:title => ["Test Title"], :read_groups => ["public"], :set => [set])
