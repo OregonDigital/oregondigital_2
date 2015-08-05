6df824c324d557f21070ac100873082f3080a2bc
Brandon Straley <straleyb@STRALEYB-MBP15.tss.oregonstate.edu>
2015-08-05 16:27:36 -0700

    updated specs to test for all possibilities

--- spec/features/update_spec.rb
+++ spec/features/update_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe "Updating a record" do
   context "when updating an existing record that has been reviewed" do
     before do
       g = review_asset
-      binding.pry
       visit edit_record_path(:id => g.id)
     end
     context "and wanting it to be re-reviewed" do
@@ -30,6 +29,23 @@ RSpec.describe "Updating a record" do
     end
     context "and wanting it to be re-reviewed" do
       before do
+        find(:css, "#to_review").set(true)
+        click_button "Save"
+      end
+
+      it "should set that record to not reviewed again" do
+        expect(GenericAsset.find(id).workflow_metadata.reviewed).to eq true
+      end
+    end
+  end
+  context "when updating an existing record that has not been reviewed" do
+    before do
+      g = asset
+      g.save
+      visit edit_record_path(g)
+    end
+    context "and not wanting it to be re-reviewed" do
+      before do
         click_button "Save"
       end
 
@@ -38,7 +54,21 @@ RSpec.describe "Updating a record" do
       end
     end
   end
- 
+  context "when updating an existing record that has been reviewed" do
+    before do
+      g = review_asset 
+      visit edit_record_path(g)
+    end
+    context "and not wanting it to be re-reviewed" do
+      before do
+        click_button "Save"
+      end
+
+      it "should set that record to not reviewed again" do
+        expect(GenericAsset.find(id).workflow_metadata.reviewed).to eq false
+      end
+    end
+  end
 end
 
 def review_asset
