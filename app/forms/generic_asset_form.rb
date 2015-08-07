9249ea443e82975f5298c3431e62a177fa687810
Trey Terrell <trey.terrell@oregonstate.edu>
2015-08-07 08:19:33 -0700

    Move delegation.

--- app/forms/generic_asset_form.rb
+++ app/forms/generic_asset_form.rb
@@ -1,6 +1,6 @@
 class GenericAssetForm
   include HydraEditor::Form
-  delegate :validators, :to => :model
+  delegate :validators, :reviewed, :to => :model
   # TODO: Put :workType back in once our editor can support URLs and/or we have
   # a "clean" graph from AF
   skip_terms = [:workType, :oembed]
