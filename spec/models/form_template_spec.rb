66021971b108faffd9a0aca253344e9125ff00e0
Jeremy Echols <jechols@uoregon.edu>
2015-08-13 14:53:28 -0700

    Prevent hidden template properties from persisting

--- spec/models/form_template_spec.rb
+++ spec/models/form_template_spec.rb
@@ -1,14 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe FormTemplate, :type => :model do
+  let(:template) { FactoryGirl.build(:form_template) }
+
   describe "#visible_property_names" do
     it "should return names of properties which are visible" do
       template = FactoryGirl.build(:form_template, :with_title, :with_desc)
-      template.save
       expect(template.visible_property_names).to contain_exactly("title", "description")
+    end
+
+    it "shouldn't return hidden properties" do
+      template = FactoryGirl.build(:form_template, :with_title, :with_desc)
       template.properties.first.visible = false
-      template.save
       expect(template.visible_property_names).to contain_exactly("description")
+    end
+  end
+
+  describe "property_names" do
+    it "should return all property names" do
+      template = FactoryGirl.build(:form_template, :with_title, :with_desc)
+      template.properties.first.visible = false
+      expect(template.property_names).to contain_exactly("title", "description")
+    end
+  end
+
+  describe "#property_names=" do
+    it "should build hidden properties" do
+      template.property_names = ["foo", "bar"]
+      expect(template.properties.length).to eq(2)
+      expect(template.property_names).to contain_exactly("foo", "bar")
+      expect(template.visible_property_names).to eq([])
+    end
+
+    it "shouldn't add properties that are already on the object" do
+      template.properties.build(:name => "foo", :visible => true)
+      template.property_names = ["foo", "bar"]
+      expect(template.properties.length).to eq(2)
+      expect(template.property_names).to contain_exactly("foo", "bar")
+      expect(template.visible_property_names).to contain_exactly("foo")
+    end
+
+    it "should update the property map" do
+      template.properties.build(:name => "foo", :visible => true)
+      expect(template.property_map.keys).to contain_exactly("foo")
+      template.property_names = ["foo", "bar"]
+      expect(template.property_map.keys).to contain_exactly("foo", "bar")
+    end
+  end
+
+  describe "before save" do
+    it "should remove all hidden properties" do
+      template.properties.build(:name => "foo", :visible => true)
+      template.property_names = ["foo", "bar"]
+      template.save
+      expect(template.property_names).to contain_exactly("foo")
     end
   end
 end
